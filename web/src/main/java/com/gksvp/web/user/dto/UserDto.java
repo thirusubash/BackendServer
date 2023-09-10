d672b07384612f94a9a962eec6104bed322c1397
THIRUMALAIVASAN K <thirumalaivasanhcl2015@outlook.com>
2023-09-10 17:39:25 +0530

    modified:   .gitignore 	modified:   web/pom.xml 	modified:   web/src/main/java/com/gksvp/web/user/dto/UserDto.java 	modified:   web/src/main/java/com/gksvp/web/user/service/impl/UserServiceImpl.java 	web/src/main/resources/application.properties

--- web/src/main/java/com/gksvp/web/user/dto/UserDto.java
+++ web/src/main/java/com/gksvp/web/user/dto/UserDto.java
@@ -1,17 +1,45 @@
 package com.gksvp.web.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Data
 public class UserDto {
+
     private Long id;
-    private String firstName;
-    private String lastName;
     private String userName;
     private String email;
+    private String countryCode;
     private String mobileNo;
-    private Date dateOfBirth;
+    private String alternateMobileNo;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private LocalDateTime createDateTime;
+    private LocalDateTime updateDateTime;
+    private Boolean mobileVerified;
+    private Boolean emailVerified;
+    private Boolean active;
+    private String url;
 
+    @JsonIgnoreProperties("user")
+    private List<AddressDto> addresses;
+
+    @JsonIgnoreProperties("user")
+    private List<UserKYCInfoDto> kycInfoList;
+
+    @JsonIgnoreProperties("user")
+    private List<GeoLocationDto> locations;
+
+    @JsonIgnoreProperties("users")
+    private Set<RoleDto> roles;
+
+    @JsonIgnoreProperties("users")
+    private Set<GroupDto> groups;
 }
